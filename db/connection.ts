01cb9fdce4dd8e8755109ccfc25bea5c930a6c9b
Victor E Gonzalez P <vemiliogp@gmail.com>
2022-06-19 13:29:04 +0200

    Last commit

--- db/connection.ts
+++ db/connection.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("node", "root", "149932099", {
-  host: "localhost",
-  dialect: "mysql",
-  //   logging: false,
-});
+const db = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    //   logging: false,
+  }
+);
 
 export default db;
